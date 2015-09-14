67d2ce7d74e279bac6f2c306c90b4dbafa8605c4
Rodrigo Olmo Velasco <rolmo@MacBook-Pro-de-Rodrigo.local>
2015-09-14 17:00:45 +0200

    Published the all resources modified by error

--- src/main/java/io/confluent/kafkarest/KafkaRestApplication.java
+++ src/main/java/io/confluent/kafkarest/KafkaRestApplication.java
@@ -84,10 +84,10 @@ public class KafkaRestApplication extends Application<KafkaRestConfig> {
     this.zkClient = zkClient;
     context = new Context(appConfig, mdObserver, producerPool, consumerManager);
     config.register(RootResource.class);
-   // config.register(new BrokersResource(context));
-    //config.register(new TopicsResource(context));
-    //config.register(new PartitionsResource(context));
-    //config.register(new ConsumersResource(context));
+    config.register(new BrokersResource(context));
+    config.register(new TopicsResource(context));
+    config.register(new PartitionsResource(context));
+    config.register(new ConsumersResource(context));
     config.register(new HelloWorldResource(context));
   }
 
